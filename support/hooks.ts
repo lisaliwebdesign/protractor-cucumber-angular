7684fe8e9a2db9d70fea71c45319ee02f6db11cd
Lisa Li <lisali@lisas-mbp.home>
2019-09-12 19:18:29 +0100

    Added typescript

---
+++ support/hooks.ts
@@ -0,0 +1,27 @@
+/*jslint node: true*/
+import { browser } from 'protractor';
+import { defineSupportCode } from "cucumber";
+import * as fs from 'fs';
+/*
+Hooks help us follow DRY principle, all the utility functions go here
+BeforeScenario, Features and screenshot hooks example provided here
+**/
+defineSupportCode(function ({registerHandler, After}) {
+
+  registerHandler('BeforeFeature', (event) => {
+    return browser.get('/app/calculator');
+  });
+
+  After((scenario, done) => {
+    if (scenario.isFailed()) {
+      return browser.takeScreenshot().then(function (base64png) {
+        let decodedImage = new Buffer(base64png, 'base64').toString('binary');
+        scenario.attach(decodedImage, 'image/png');
+      }, (err) => {
+        done(err);
+      });
+    } else {
+      done();
+    }
+  });
+})
